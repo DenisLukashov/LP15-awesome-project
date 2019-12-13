c92c8ed276ffdce30cf3d8651d800150e55dcbc3
Omar Flow <academboy74@gmail.com>
2019-12-13 17:05:56 +0300

    fix routes

--- awesomeapp/__init__.py
+++ awesomeapp/__init__.py
@@ -6,8 +6,4 @@ def create_app():
     app = Flask(__name__)
     app.config.from_pyfile('config.py')
     db.init_app(app)
-
-    @app.route('/')
-    def hello():
-        return 'Hello'
     return app
