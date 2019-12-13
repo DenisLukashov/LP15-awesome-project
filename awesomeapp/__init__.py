4d7b69d4eb39f041d06343ba41713bfa14c1c44d
Omar Flow <academboy74@gmail.com>
2019-12-13 16:38:37 +0300

    init awesome app

---
+++ awesomeapp/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+from .models import db
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_pyfile('config.py')
+    db.init_app(app)
+
+    @app.route('/')
+    def hello():
+        return 'Hello'
+    return app
--- users.py
+++
@@ -1,9 +0,0 @@
-# work with users
-class User:
-    def __init__(self, username, email):
-        self.username = username
-	self.email = email
-	
-def sosi(user):
-    if user:
-        print('ok')
