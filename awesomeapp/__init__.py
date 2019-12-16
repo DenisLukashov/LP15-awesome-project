f9d6ef0361b0f724dd2648e3b4cf3cee826c2d10
Omar Flow <academboy74@gmail.com>
2019-12-16 19:52:15 +0300

    replace and add to gitignore config.py, add config.py.template, create migrations, install flask-migrate, init migrate object

--- awesomeapp/__init__.py
+++ awesomeapp/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask, render_template
+from flask_migrate import Migrate
 
 from .models import db
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_pyfile('config.py')
+    app.config.from_pyfile('../config.py')
     db.init_app(app)
+    migrate = Migrate(app, db)
     return app
