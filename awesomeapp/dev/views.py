87c6ac48b44a042265c783784a65ea8ecd6e90f7
Denis_L <den3900@mail.ru>
2020-01-07 00:24:19 +0300

    #1 add visit.html

--- awesomeapp/dev/views.py
+++ awesomeapp/dev/views.py
@@ -1,5 +1,11 @@
-from flask import send_from_directory, Blueprint
-
+from flask import (
+    send_from_directory, 
+    Blueprint,
+    render_template, 
+    redirect, 
+    url_for
+)
+from flask_login import current_user
 from config import Config
 
 blueprint = Blueprint('dev', __name__, template_folder='templates')
@@ -8,3 +14,10 @@ blueprint = Blueprint('dev', __name__, template_folder='templates')
 @blueprint.route('/static/<path>/<filename>')
 def send_static(path, filename):
     return send_from_directory(f'{Config.STATIC_FOLDER}/{path}', filename)
+
+@blueprint.route('/')
+def start_page():
+    if current_user.is_authenticated:
+        return redirect(url_for('index.index'))
+    else:
+        return redirect(url_for('user.visit'))
