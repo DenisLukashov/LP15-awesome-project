9ade6338abcda03f35a7d46bca607daede1d65e5
Omar Flow <academboyhtml@gmail.com>
2020-01-10 12:01:01 +0300

    index template

--- awesomeapp/dev/views.py
+++ awesomeapp/dev/views.py
@@ -14,6 +14,7 @@ blueprint = Blueprint('dev', __name__, template_folder='templates')
 @blueprint.route('/static/<path>/<filename>')
 def send_static(path, filename):
     return send_from_directory(f'{Config.STATIC_FOLDER}/{path}', filename)
+    # return send_from_directory(f'static/{path}', filename)
 
 @blueprint.route('/')
 def start_page():
--- awesomeapp/index/views.py
+++ awesomeapp/index/views.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
+from awesomeapp.equipment.models import Equipment
 blueprint = Blueprint('index', __name__, template_folder='templates', url_prefix='/index')
 
 
 @blueprint.route('/')
 def index():
-    return render_template('index/index.html', title='Главная страница')
+    equips = Equipment.query.filter(Equipment.user_id == current_user.id).all()
+    for i in equips:
+        print(i.name)
+    return render_template('index/index.html', title='Главная страница', equips=equips)
