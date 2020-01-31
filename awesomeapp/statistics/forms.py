accd936ad1c52fe44f5a436a663af338a643ba89
Denis_L <den3900@mail.ru>
2020-01-31 15:59:31 +0300

    site layout for old asus

--- awesomeapp/statistics/forms.py
+++ awesomeapp/statistics/forms.py
@@ -226,7 +226,8 @@ class StatisticsForm(FlaskForm):
         'О тренировке',
         render_kw={
             'class': 'form-control',
-            'placeholder': 'О тренировке'
+            'placeholder': 'О тренировке',
+            'rows': '1'
         }
     )
 
