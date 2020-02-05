3eeee015fe79748e2e8548e8a2be21d91111b892
Omar Flow <academboyhtml@gmail.com>
2020-02-05 11:38:20 +0300

    refactoring

--- add_equipmnet_type.py
+++ add_equipmnet_type.py
@@ -2,7 +2,7 @@ from awesomeapp import app
 with app.app_context():
   from awesomeapp.equipment.models import EquipmentType
   from awesomeapp.extensions import db
-  sport = ['Ходьба', 'Бег', 'Беговая дорожка', 'Лыжи', 'Лыжероллеры', 'Коньки/Ролики', 'Велосипед', 'Велотренажер']
-  for i in sport:
+  sports = ['Ходьба', 'Бег', 'Беговая дорожка', 'Лыжи', 'Лыжероллеры', 'Коньки/Ролики', 'Велосипед', 'Велотренажер']
+  for sport in sports:
     db.session.add(EquipmentType(type_name=i))
   db.session.commit()
