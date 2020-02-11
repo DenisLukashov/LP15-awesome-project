bd23c56737c95b6c97153870edbe9cf468c35ac6
Omar Flow <academboyhtml@gmail.com>
2020-02-11 19:15:16 +0300

    refactoring

--- tests/unit/test_healthcheck.py
+++ tests/unit/test_healthcheck.py
@@ -7,6 +7,6 @@ endpoints = ('user.register', 'user.logout', 'user.login',
 
 @pytest.mark.parametrize('endpoint', endpoints)
 def test_pages_healthcheck(client, init_database, endpoint):
-    """ Проверка конечных точек на доступность"""
+    """ Проверка конечных точек на доступность."""
     response = client.get(url_for(endpoint), follow_redirects=True)
     assert response.status_code == 200
