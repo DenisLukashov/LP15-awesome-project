3c6583972c872169121f2678105f455174d813a8
Denis_L <den3900@mail.ru>
2020-02-12 21:00:37 +0300

    #9 travis

--- add_equipmnet_type.py
+++ add_equipmnet_type.py
@@ -2,7 +2,16 @@ from awesomeapp import app
 with app.app_context():
     from awesomeapp.equipment.models import EquipmentType
     from awesomeapp.extensions import db
-    sports = ['Ходьба', 'Бег', 'Беговая дорожка', 'Лыжи', 'Лыжероллеры', 'Коньки/Ролики', 'Велосипед', 'Велотренажер']
+    sports = [
+        'Ходьба',
+        'Бег',
+        'Беговая дорожка',
+        'Лыжи',
+        'Лыжероллеры',
+        'Коньки/Ролики',
+        'Велосипед',
+        'Велотренажер'
+    ]
     for sport in sports:
         db.session.add(EquipmentType(type_name=sport))
     db.session.commit()
